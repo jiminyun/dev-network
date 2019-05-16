570afc2bdd82aa539d45a95df25b84c788638127
Jimin <jiminyun7@gmail.com>
2019-05-15 23:10:51 -0400

    prepare for deployment

---
+++ config/__key.js
@@ -0,0 +1,6 @@
+module.exports = {
+  monoURI:
+    "mongodb+srv://jiminyun:pu2889pu_!@cluster0-pfwpk.mongodb.net/test?retryWrites=true",
+  secretOrKey: "secret"
+  //useNewUrlParser: true,
+};
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 const app = express();
 
 // Connect to MongoDB
@@ -14,6 +15,17 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/upload", require("./routes/api/upload"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
